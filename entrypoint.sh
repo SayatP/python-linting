22b917fb0afc489672943e4d70529471bf2ae102
Taguhi <taguhiasatryan@gmail.com>
2020-11-25 14:35:40 +0400

    add skip file for isort

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-flake8 --max-line-length=88 --exclude=migrations --format=pylint && isort . --check --diff && echo "Your code looks perfect ✨✨"
+flake8 --max-line-length=88 --exclude=migrations --format=pylint && isort . --check --diff -s migrations && echo "Your code looks perfect ✨✨"

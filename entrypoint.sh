166f9474a973b22dddcfd712f291f5573b28b46e
SayatP <43889425+SayatP@users.noreply.github.com>
2020-12-08 16:29:17 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-flake8 --max-line-length=120 --exclude=migrations,.git,__pycache__ --format=pylint && isort . --check --diff -s migrations .git && echo "Your code looks perfect ✨✨"
+flake8 --max-line-length=120 --exclude=migrations,.git,__pycache__ --format=pylint && isort . --check --diff -s migrations -s .git && echo "Your code looks perfect ✨✨"
